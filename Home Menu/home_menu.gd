73e6ae3b71702c94746c52ba0c11ce1712c43dc9
DasParijat <143519231+DasParijat@users.noreply.github.com>
2025-08-05 14:05:58 -0400

    Created fake loading in home menu + updated slider grabber icon

--- Home Menu/home_menu.gd
+++ Home Menu/home_menu.gd
@@ -1,5 +1,17 @@
 extends Control
 class_name HomeMenu
 
+@onready var play_button : Button = $MarginContainer/VSplitContainer/MarginContainer/MarginContainer/PlayButton
+
 func _on_play_button_pressed() -> void:
+	## Set up loading information
+	play_button.text = "Loading..."
+	play_button.icon = null
+	play_button.disabled = true
+	
+	# Reason for fake loading, is that there is a bit of lag
+	# when it changes to game scene, so this timeout statement
+	# allows the loading text to show and the lag seem like part of the loading
+	await get_tree().create_timer(0.2).timeout
+	
 	GScene.change_scene(GScene.GAME)
